08a979b46067659e1b74663a8ba744f4d79f2d2b
Tem <t3mir1an@gmail.com>
2025-11-16 06:05:11 -0600

    Initial Commit

---
+++ DreamGrid/DreamGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamGridApp.swift
+//  DreamGrid
+//
+//  Created by Tem on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DreamGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
